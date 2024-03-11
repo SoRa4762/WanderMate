ff27c13ffea81c6339659d0d5d379c368fecdb2f
SoRa <sonamraisx007@gmail.com>
2024-03-11 22:24:33 +0545

    probably landing page fixes

--- src/elements/Headers/Landing.jsx
+++ src/elements/Headers/Landing.jsx
@@ -6,12 +6,15 @@ const LandingHeader = () => {
     <>
       <div className="h-[10vh] w-full flex justify-between items-center">
         <div className="w-1/3 sm:flex hidden">
-          <h1 className="pl-4 md:pl-8 text-yellow-500 font-bold text-2xl lg:text-3xl">
+          <h1
+            className="pl-4 md:pl-8 text-yellow-500 font-bold text-2xl lg:text-4xl"
+            style={{ textShadow: "4px 4px 4px rgba(0, 0, 0, 0.4)" }}
+          >
             Explore.
           </h1>
         </div>
         <Navigation />
-        <div className="h-36 md:h-52 w-[42%] sm:w-1/3">
+        <div className="h-36 md:h-52 lg:h-64 w-[42%] sm:w-1/3">
           <img
             className="h-full w-full object-cover object-center overflow-hidden"
             style={{
