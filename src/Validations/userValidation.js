9f5c11e826235491b5526b0212969a22b4d3d0c2
SoRa <sonamraisx007@gmail.com>
2024-03-21 22:42:24 +0545

    validation added

---
+++ src/Validations/userValidation.js
@@ -0,0 +1,24 @@
+import * as yup from "yup";
+
+export const signUpSchema = yup.object().shape({
+  username: yup.string().required("Username cannot be empty"),
+  email: yup
+    .string()
+    .email("Enter a valid email")
+    .required("Email cannot be empty"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(50, "password must be at most 50 characters")
+    .required("Password cannot be empty"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords donot match")
+    .required("Confirm password cannot be empty"),
+  iAgree: yup
+    .bool()
+    .oneOf(
+      [true],
+      "You must agree to the terms and conditions before sigining up!"
+    ),
+});
