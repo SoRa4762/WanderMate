d7874e24490bacd6d9fdf33728dd37464e21c02c
SoRa <sonamraisx007@gmail.com>
2024-03-22 23:43:06 +0545

    validation added to sign in

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,26 +1,41 @@
 import { useState } from "react";
 import SignInImage from "../assets/undraw_signin.svg";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signInSchema } from "../Validations/userValidation";
 
 const SignIn = () => {
-  const [signInValues, setSignInValues] = useState({});
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setSignInValues((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-
-    // console.log(signInValues);
-  };
+  // const [signInValues, setSignInValues] = useState({});
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setSignInValues((prevData) => ({
+  //     ...prevData,
+  //     [name]: value,
+  //   }));
+  // console.log(signInValues);
+  // };
 
   //for submit i guess
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const email = e.target[0].value;
-    const password = e.target[1].value;
-    const rememberMe = e.target[2].value;
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const email = e.target[0].value;
+  //   const password = e.target[1].value;
+  //   const rememberMe = e.target[2].value;
 
-    await console.log(email, password, rememberMe);
+  //   await console.log(email, password, rememberMe);
+  // };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(signInSchema),
+  });
+
+  //onSubmit
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
@@ -33,41 +48,54 @@ const SignIn = () => {
           {/* sign in form */}
           <form
             action="submit"
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit(onSubmit)}
             className="flex flex-col gap-2 md:gap-4 lg:gap-6 justify-end lg:justify-center items-center pl-8 pr-8 sm:pl-12 sm:pr-12"
           >
             <h1 className="text-blue-600 text-3xl md:text-4xl lg:text-5xl font-bold">
               Sign In
             </h1>
 
-            <input
-              className="h-10 md:h-14 pl-4 w-full border-2 border-blue-600 rounded-md focus:border-blue-600"
-              type="text"
-              placeholder="Username"
-              name="username"
-              id="username"
-              onChange={handleChange}
-              value={signInValues["username"]}
-            />
+            <div className="w-full">
+              <input
+                className="h-10 md:h-14 pl-4 w-full border-2 border-blue-600 rounded-md focus:border-blue-600"
+                type="text"
+                placeholder="Username"
+                name="username"
+                id="username"
+                {...register("username")}
+                // onChange={handleChange}
+                // value={signInValues["username"]}
+              />
+              <p className="text-xs lg:text-sm text-red-600 font-semibold pt-1">
+                {errors.username?.message}
+              </p>
+            </div>
 
             {/*if you add eye button, make sure to turn the type to text and back */}
-            <input
-              className="h-10 md:h-14 pl-4 w-full border-2 border-blue-600 rounded-md focus:border-blue-600"
-              type="password"
-              placeholder="Password"
-              name="password"
-              id="password"
-              value={signInValues["password"]}
-              onChange={handleChange}
-            />
+            <div className="w-full">
+              <input
+                className="h-10 md:h-14 pl-4 w-full border-2 border-blue-600 rounded-md focus:border-blue-600"
+                type="password"
+                placeholder="Password"
+                name="password"
+                id="password"
+                {...register("password")}
+                // value={signInValues["password"]}
+                // onChange={handleChange}
+              />
+              <p className="text-xs lg:text-sm text-red-600 font-semibold pt-1">
+                {errors.password?.message}
+              </p>
+            </div>
 
             <div className="flex gap-2 justify-start w-full">
               <input
                 type="checkbox"
                 name="condition"
                 id="condition"
-                value={signInValues["condition"]}
-                onChange={handleChange}
+                {...register("remember")}
+                // value={signInValues["condition"]}
+                // onChange={handleChange}
               />
               <p>Remember Me </p>
             </div>
