b272560a9a639844d747208976f5506ba3b7386f
SoRa <sonamraisx007@gmail.com>
2024-04-22 14:41:43 +0545

    added animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import NavigationOverall from "../elements/Navigation/NavigationOverall";
-import { hotels, thingsToDo, homeItems } from "../helper/data";
-import TopHotels from "../assets/bg4.jpg";
+import { hotels, travelPackages, thingsToDo, homeItems } from "../helper/data";
+import TopHotels from "../assets/bg2.jpg";
 import TopTravelPackage from "../assets/bg5.jpg";
 import ThingsToDo from "../assets/bg.jpg";
 import { CiHeart } from "react-icons/ci";
@@ -17,25 +17,25 @@ const Home = () => {
 
         <div className="h-full w-full flex flex-col">
           {/* hotels */}
-          <div className="h-full w-full flex flex-col pt-4 sm:px-10">
+          <div className="h-full w-full flex flex-col pt-4 xl:px-10">
             {/* heading */}
             <div className="flex gap-2 items-center pt-8">
               <img
-                className="h-8 w-8 rounded-full"
+                className="h-8 w-8 rounded-full cursor-pointer"
                 src={TopHotels}
                 alt="randomIcon"
               />
-              <h1 className="font-bold text-md sm:text-lg lg:text-xl">
+              <h1 className="font-bold text-md sm:text-lg lg:text-xl cursor-pointer">
                 Top Hotels
               </h1>
             </div>
             {/* cards */}
-            <div className="h-full w-full grid grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+            <div className="h-full w-full grid grid-cols-2 xl:grid-cols-4 gap-4 pt-4">
               {hotels.map((item) => (
                 <>
                   <div
                     key={item.id}
-                    className="h-[50vh] w-full flex flex-col justify-between bg-red-200 rounded-lg cursor-pointer"
+                    className="h-[30vh] sm:h-[40vh] xl:h-[50vh] w-full flex flex-col justify-between bg-red-200 rounded-lg cursor-pointer drop-shadow-lg  hover:-translate-y-2 transition-all ease-in-out duration-300"
                     style={{
                       background: ` url(${item.img})`,
                       backgroundSize: "cover",
@@ -44,7 +44,7 @@ const Home = () => {
                     <div className="h-8 w-8 m-2 rounded-full bg-white flex justify-center items-center">
                       <CiHeart size={24} />
                     </div>
-                    <div className="backdrop-blur-sm rounded-b-lg px-4 py-4 flex justify-between text-white font-bold">
+                    <div className="backdrop-blur-sm text-xs sm:text-sm md:text-base rounded-b-lg px-4 py-4 flex justify-between text-white font-bold">
                       <p>{item.name}</p>
                       <p>${item.price}</p>
                     </div>
@@ -55,25 +55,25 @@ const Home = () => {
           </div>
 
           {/* top travel packages */}
-          <div className="h-full w-full flex flex-col sm:px-10">
+          <div className="h-full w-full flex flex-col xl:px-10">
             {/* heading */}
             <div className="flex gap-2 items-center pt-8">
               <img
-                className="h-8 w-8 rounded-full"
+                className="h-8 w-8 rounded-full cursor-pointer"
                 src={TopTravelPackage}
                 alt="randomIcon"
               />
-              <h1 className="font-bold text-md sm:text-lg lg:text-xl">
+              <h1 className="font-bold text-md sm:text-lg lg:text-xl cursor-pointer">
                 Top Travel Packages
               </h1>
             </div>
             {/* cards */}
-            <div className="h-full w-full grid grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-              {hotels.map((item) => (
+            <div className="h-full w-full grid grid-cols-2 xl:grid-cols-4 gap-4 pt-4">
+              {travelPackages.map((item) => (
                 <>
                   <div
                     key={item.id}
-                    className="h-[50vh] w-full flex flex-col justify-between bg-red-200 rounded-lg cursor-pointer"
+                    className="h-[30vh] sm:h-[40vh] xl:h-[50vh] w-full flex flex-col justify-between bg-red-200 rounded-lg cursor-pointer drop-shadow-lg  hover:-translate-y-2 transition-all ease-in-out duration-300"
                     style={{
                       background: ` url(${item.img})`,
                       backgroundSize: "cover",
@@ -82,7 +82,7 @@ const Home = () => {
                     <div className="h-8 w-8 m-2 rounded-full bg-white flex justify-center items-center">
                       <CiHeart size={24} />
                     </div>
-                    <div className="backdrop-blur-sm rounded-b-lg px-4 py-4 flex justify-between text-white font-bold">
+                    <div className="backdrop-blur-sm rounded-b-lg px-4 py-4 flex justify-between text-xs sm:text-sm md:text-base text-white font-bold">
                       <p>{item.name}</p>
                       <p>${item.price}</p>
                     </div>
@@ -93,25 +93,25 @@ const Home = () => {
           </div>
 
           {/* things to do */}
-          <div className="h-full w-full flex flex-col sm:px-10">
+          <div className="h-full w-full flex flex-col xl:px-10">
             {/* heading */}
             <div className="flex gap-2 items-center pt-8">
               <img
-                className="h-8 w-8 rounded-full"
+                className="h-8 w-8 rounded-full cursor-pointer"
                 src={ThingsToDo}
                 alt="randomIcon"
               />
-              <h1 className="font-bold text-md sm:text-lg lg:text-xl">
+              <h1 className="font-bold text-md sm:text-lg lg:text-xl cursor-pointer">
                 Things To Do
               </h1>
             </div>
             {/* cards */}
-            <div className="h-full w-full grid grid-cols-2 lg:grid-cols-4 gap-4 py-4">
+            <div className="h-full w-full grid grid-cols-2 xl:grid-cols-4 gap-4 py-4">
               {thingsToDo.map((item) => (
                 <>
                   <div
                     key={item.id}
-                    className="h-[50vh] w-full flex flex-col justify-between bg-red-200 rounded-lg cursor-pointer"
+                    className="h-[30vh] sm:h-[40vh] xl:h-[50vh] w-full flex flex-col justify-between bg-red-200 rounded-lg cursor-pointer drop-shadow-lg hover:-translate-y-2 transition-all ease-in-out duration-300"
                     style={{
                       background: ` url(${item.img})`,
                       backgroundSize: "cover",
@@ -120,7 +120,7 @@ const Home = () => {
                     <div className="h-8 w-8 m-2 rounded-full bg-white flex justify-center items-center">
                       <CiHeart size={24} />
                     </div>
-                    <div className="backdrop-blur-sm rounded-b-lg px-4 py-4 flex justify-between text-white font-bold">
+                    <div className="backdrop-blur-sm rounded-b-lg px-4 py-4 text-xs sm:text-sm md:text-base flex justify-between text-white font-bold">
                       <p>{item.name}</p>
                       <p>${item.price}</p>
                     </div>
--- src/elements/Footers/FFAll.jsx
+++ src/elements/Footers/FFAll.jsx
@@ -3,48 +3,48 @@ import { ffAll } from "../../helper/data";
 const FFAll = () => {
   return (
     <>
-      <div className="flex flex-col sm:mx-10 my-10 mt-15 bg-white shadow-2xl">
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 px-12 ">
-          <div className="flex flex-col items-center md:items-start gap-2">
-            <h3 className="text-gray-800 font-semibold text-base md:text-xl  pb-2">
+      <div className="flex flex-col xl:mx-10 my-10 mt-15 bg-white shadow-2xl">
+        <div className="grid grid-cols-2 lg:grid-cols-3 px-4 sm:px-12 ">
+          <div className="flex flex-col items-start gap-2">
+            <h3 className="text-gray-800 font-semibold text-sm md:text-xl pb-1 sm:pb-2">
               About WanderMate
             </h3>
             {ffAll.slice(0, 5).map((about) => (
               <>
-                <p className="text-sm md:text-base">
+                <p className="text-xs md:text-base hover:text-blue-600 hover:font-semibold">
                   <a href={about.link}>{about.name}</a>
                 </p>
               </>
             ))}
           </div>
 
-          <div className="flex flex-col gap-2 items-center md:items-start  my-6 md:my-0">
-            <h3 className="text-gray-800 font-semibold text-base md:text-xl  pb-2">
+          <div className="flex flex-col gap-2 items-start md:my-0">
+            <h3 className="text-gray-800 font-semibold text-sm md:text-xl pb-1 sm:pb-2">
               Explore
             </h3>
             {ffAll.slice(5, 9).map((explore) => (
               <>
-                <p className="text-sm md:text-base">
+                <p className="text-xs md:text-base hover:text-blue-600 hover:font-semibold">
                   <a href={explore.link}>{explore.name}</a>
                 </p>
               </>
             ))}
           </div>
 
-          <div className="flex flex-col gap-2 items-center md:items-start my-0 md:my-6 lg:my-0">
-            <h3 className="text-gray-800 font-semibold text-base md:text-xl pb-2">
+          <div className="flex flex-col gap-2 items-start my-6 lg:my-0">
+            <h3 className="text-gray-800 font-semibold text-sm md:text-xl pb-1 sm:pb-2">
               Trip-Advisor Sites
             </h3>
             {ffAll.slice(-4).map((extra) => (
               <>
-                <p className="text-sm md:text-base">
+                <p className="text-xs md:text-base hover:text-blue-600 hover:font-semibold">
                   <a href={extra.link}>{extra.name}</a>
                 </p>
               </>
             ))}
           </div>
         </div>
-        <p className="text-center text-gray-900 font-semibold text-sm md:text-base my-6">
+        <p className="text-center text-gray-900 font-semibold text-sm md:text-base mt-2 mb-6">
           &copy; 2024 WanderMate LLC All rights reserved
         </p>
       </div>
