51fb2679693e5041c35e90629ce812b071ef076a
SoRa <sonamraisx007@gmail.com>
2024-04-24 22:21:17 +0545

    destination and home page fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,73 @@
+import { hotels, travelPackages, topDestinations } from "../helper/data";
 import NavigationOverall from "../elements/Navigation/NavigationOverall";
-import { hotels, travelPackages, thingsToDo, homeItems } from "../helper/data";
-import TopHotels from "../assets/bg2.jpg";
-import TopTravelPackage from "../assets/bg5.jpg";
-import ThingsToDo from "../assets/bg.jpg";
-import Carousel from "../elements/MyCarousel";
 import FFAll from "../elements/Footers/FFAll";
 import Cards from "../elements/Cards";
+import TopHotels from "../assets/bg2.jpg";
+import TopTravelPackage from "../assets/bg5.jpg";
+import TopDestination from "../assets/bg.jpg";
+import bgImg from "../assets/headerImg9.jpg";
+import { IoSearch } from "react-icons/io5";
 
 const Home = () => {
   return (
     <>
       <div className="px-8 sm:px-12 md:px-16 lg:px-20">
         <NavigationOverall />
-        {/* carousel */}
-        <Carousel data={homeItems} />
+
+        {/* search section */}
+        <div
+          className="h-[40vh] relative sm:h-[55vh] md:h-[70vh] lg:h-[75vh] mt-8 w-full transition-all duration-300 ease-in-out rounded-lg"
+          style={{
+            background: `url(${bgImg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="h-[12%] lg:w-[39%] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] bg-white flex justify-center items-center gap-2 p-1 md:p-4 rounded-lg">
+            <input
+              className="h-[90%] w-[80%] lg:w-[90%] border-none outline-none text-xs sm:text-sm md:text-base lg:text-lg"
+              placeholder="Search Your Places, Destination..."
+              type="text"
+            />
+            <div className="h-5 w-5 sm:h-7 sm:w-7 md:h-10 md:w-10 lg:h-11 lg:w-11 flex items-center justify-center bg-blue-600 rounded-full cursor-pointer">
+              <IoSearch
+                className="lg:flex xs:hidden hidden"
+                size={22}
+                color="white"
+              />
+              <IoSearch
+                className="md:flex lg:hidden hidden"
+                size={18}
+                color="white"
+              />
+              <IoSearch
+                className="sm:flex md:hidden hidden"
+                size={16}
+                color="white"
+              />
+              <IoSearch className="flex sm:hidden" size={12} color="white" />
+            </div>
+          </div>
+        </div>
 
         <div className="h-full w-full flex flex-col">
+          {/* destinations */}
+          <div className="h-full w-full flex flex-col pt-4 xl:px-10">
+            {/* heading */}
+            <div className="flex gap-2 items-center pt-8">
+              <img
+                className="h-8 w-8 rounded-full cursor-pointer"
+                src={TopDestination}
+                alt="randomIcon"
+              />
+              <h1 className="font-bold text-md sm:text-lg lg:text-xl cursor-pointer">
+                Top Destinations
+              </h1>
+            </div>
+            {/* cards */}
+            <Cards data={topDestinations} />
+          </div>
+
           {/* hotels */}
           <div className="h-full w-full flex flex-col pt-4 xl:px-10">
             {/* heading */}
@@ -49,24 +101,6 @@ const Home = () => {
             {/* cards */}
             <Cards data={travelPackages} />
           </div>
-
-          {/* things to do */}
-          <div className="h-full w-full flex flex-col xl:px-10">
-            {/* heading */}
-            <div className="flex gap-2 items-center pt-8">
-              <img
-                className="h-8 w-8 rounded-full cursor-pointer"
-                src={ThingsToDo}
-                alt="randomIcon"
-              />
-              <h1 className="font-bold text-md sm:text-lg lg:text-xl cursor-pointer">
-                Things To Do
-              </h1>
-            </div>
-            {/* cards */}
-            <Cards data={thingsToDo} />
-          </div>
-
           <FFAll />
         </div>
       </div>
